71d79b45651b140ab192dd2fdb3f451648686911
Aysima Karcaaltincaba <akarcaal@cisco.com>
2025-11-22 22:19:18 +0300

    remove unused files

--- python_server/latent_space_class.py
+++ python_server/latent_space_class.py
@@ -410,7 +410,7 @@ class LatentSpace():
         """Delegate to DataTrainer for backward compatibility."""
         try:
             # Support both package and script execution contexts
-            from data_trainer import DataTrainer as _DT
+            from clustering.data_trainer import DataTrainer as _DT
         except Exception:
             _DT = DataTrainer
         trainer = _DT()
